a90d2400fe49ce368b8b45a8ccac67a7d78e7112
hurtsky <sabmar_com@msn.com>
2015-09-12 11:11:20 +0800

    sweep2wake: add sweep2sleep and code update
    s2w: Add bidirectional sweep2sleep
    
    Signed-off-by: Savoca <adeddo27@gmail.com>
    
    s2w: Use s2s-only (s2w_switch == 2) properly
    
    Signed-off-by: Savoca <adeddo27@gmail.com>
    
    s2w: Adjust left->right definitions
    
    Signed-off-by: Savoca <adeddo27@gmail.com>
    
    s2w: remove all s2sonly code
    
    Signed-off-by: Savoca <adeddo27@gmail.com>
    
    s2w: pocket mode should only for sweep2wake
    Signed-off-by: hurtsky <sabmar_com@msn.com>

--- drivers/input/misc/ct406.c
+++ drivers/input/misc/ct406.c
@@ -563,7 +563,7 @@ static void ct406_prox_mode_uncovered(struct ct406_data *ct)
 	if (piht > ct->pdata_max)
 		piht = ct->pdata_max;
 #ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
-if (dt2w_switch > 0) {
+if (s2w_switch == 1 || dt2w_switch > 0) {
 	prox_covered = false;
 	if (screen_suspended) {
 		touch_resume();
@@ -590,7 +590,7 @@ static void ct406_prox_mode_covered(struct ct406_data *ct)
 	ct->prox_high_threshold = piht;
 	ct406_write_prox_thresholds(ct);
 #ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
-	if (dt2w_switch > 0) {
+	if (s2w_switch == 1 || dt2w_switch > 0) {
 		prox_covered = true;
 		if (screen_suspended) {
 		touch_suspend();
@@ -1747,7 +1747,7 @@ static int ct406_probe(struct i2c_client *client,
 		goto error_revision_read_failed;
 	}
 #ifdef CONFIG_TOUCHSCREEN_PREVENT_SLEEP
-	if (s2w_switch > 0 || dt2w_switch > 0)
+	if (s2w_switch == 1 || dt2w_switch > 0)
 		ct406_enable_prox(ct);
 #endif
 
