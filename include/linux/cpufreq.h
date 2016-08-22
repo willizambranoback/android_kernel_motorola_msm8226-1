c6862bf4d152db4b1a30309b274f7413e0af2d36
BryanByteZero <maxtrider@gmail.com>
2016-08-21 21:06:51 -0300

    Trying to fix YankActive

--- include/linux/cpufreq.h
+++ include/linux/cpufreq.h
@@ -378,7 +378,6 @@ extern struct cpufreq_governor cpufreq_gov_interactive;
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_CONSERVATIVE)
 extern struct cpufreq_governor cpufreq_gov_conservative;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_conservative)
-// Additional governors
 #elif defined(CONFIG_CPU_FREQ_DEFAULT_GOV_YANKDEMAND)
 extern struct cpufreq_governor cpufreq_gov_yankdemand;
 #define CPUFREQ_DEFAULT_GOVERNOR	(&cpufreq_gov_yankdemand)
